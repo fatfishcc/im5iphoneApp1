861eb3f84d11346ca227f9eae7c0168643058948
theloshackers <theloshackers@gmail.com>
2023-05-06 09:08:51 +1000

    first commit

---
+++ im5iphoneApp1/im5iphoneApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  im5iphoneApp1App.swift
+//  im5iphoneApp1
+//
+//  Created by admin on 6/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct im5iphoneApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
