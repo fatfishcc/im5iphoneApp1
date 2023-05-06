863e0ad1f11a3088fb9299127acf81114cbda706
theloshackers <theloshackers@gmail.com>
2023-05-06 10:29:11 +1000

    update

---
+++ im5iphoneApp1-iOS/im5iphoneApp1-iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  im5iphoneApp1-iOS
+//
+//  Created by admin on 6/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, worldex!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ im5iphoneApp1-iOS/im5iphoneApp1-iOS/im5iphoneApp1_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  im5iphoneApp1_iOSApp.swift
+//  im5iphoneApp1-iOS
+//
+//  Created by admin on 6/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct im5iphoneApp1_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
